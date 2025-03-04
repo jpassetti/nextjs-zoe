cf9b51ce44b47af9be3bfd8fc3963fe93ea61d0f
Jeff Passetti <jpassetti@gmail.com>
2025-03-04 13:50:41 -0500

    wordmark issues

--- components/brand/Wordmark/index.tsx
+++ components/brand/Wordmark/index.tsx
@@ -4,7 +4,7 @@ import styles from "./wordmark.module.scss";
 const Wordmark = () => {
  return (
   <Image
-   src="/brand/complete-wordmark.svg"
+   src="/brand/complete-wordmark-2.svg"
    alt="Transform with Irini"
    width={664}
    height={113}
