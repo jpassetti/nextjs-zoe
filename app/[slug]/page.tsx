9a625ac228bc16c153e48826418fba3dc3c3492c
Jeff Passetti <jpassetti@gmail.com>
2025-04-25 16:22:33 -0400

    bannedSlugs

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -6,6 +6,7 @@ import { notFound } from "next/navigation";
 import ColumnsSection from "@/components/custom/ColumnsSection";
 import Showcase from "@/components/custom/Showcase";
 import CTA from "@/components/custom/CTA";
+import SanityPage from "@/components/custom/SanityPage";
 
 export async function generateMetadata({ params }: { params: { slug: string } }) {
   const pageData: PageData = await getPage(params.slug);
@@ -41,11 +42,11 @@ export default async function Page({ params }: { params: Promise<{ slug: string 
   }
 
   const { sections, callToAction } = pageData;
-  
+  const bannedSlugs = ["about-me", "contact"];
   return (
     <Fragment>
-
-      {sections && sections.length > 0 &&
+      {bannedSlugs.includes(slug) && <SanityPage page={pageData} />}
+      {!bannedSlugs.includes(slug) && sections && sections.length > 0 &&
         sections.map((section, index) => {
           switch (section._type) {
             case "showcaseSection":
